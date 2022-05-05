fd1a2543795c6ad3796983079b9aa55d24250a13
MoisesAzevedo <moisesdev2022@gmail.com>
2022-05-05 01:28:13 -0300

    Estilização; e inicio do desenvolvimento js.

--- script.js
+++ script.js
@@ -6,9 +6,12 @@
 
 function noticias(){
 
-    document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> `
-    document.querySelector('body').innerHTML += ` <button onclick="stopNews()" class="botao2">Stop News</button> `
-    document.querySelector('body').innerHTML += ` <iframe src="https://www.tecmundo.com.br/" class="iframe"></iframe> `    
+    /* document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> ` */
+    document.querySelector('body').innerHTML += ` 
+        <div style="width: 100%" class="janela-noticia">
+            <button onclick="stopNews()"  class="botao2">Stop News</button> 
+            <iframe src="https://www.tecmundo.com.br/" class="iframe"></iframe> 
+        </div> `    
 
  //   if  (noticias() == true) {
   //      noticias() == false
@@ -17,20 +20,39 @@ function noticias(){
 
 function noticias2(){
 
-    document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> `
-    document.querySelector('body').innerHTML += ` <button onclick="stopNews()" class="botao2">Stop News</button> `
-    document.querySelector('body').innerHTML += ` <iframe src="https://www.cnnbrasil.com.br/" class="iframe"></iframe> `
-    
+    /* document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> ` */
+    document.querySelector('body').innerHTML += ` 
+        <div style="width: 100%" class="janela-noticia">
+            <button onclick="stopNews()" class="botao2">Stop News</button> 
+            <iframe src="https://www.cnnbrasil.com.br/" class="iframe"></iframe> 
+        </div> `
+
 }
 
 function noticias3(){
 
-    document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> `
-    document.querySelector('body').innerHTML += ` <button onclick="stopNews()" class="botao2">Stop News</button> `
-    document.querySelector('body').innerHTML += ` <iframe src="https://www.infomoney.com.br/" class="iframe"></iframe> `
-    
+    /* document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> ` */
+    document.querySelector('body').innerHTML += ` 
+        <div style="width: 100%" class="janela-noticia">
+            <button onclick="stopNews()"  class="botao2">Stop News</button> 
+            <iframe src="https://www.infomoney.com.br/" class="iframe"></iframe> 
+        </div> `
+
 }
 
 // function stopNews(){
  //  noticias() == false      //Não está dando certo
 // }
+
+var janelaNoticia = document.querySelector(".janela-noticia")
+
+function stopNews(){
+    console.log("oi")
+}
+
+/* var botao2 = document.querySelector(".botao2")
+
+botao2.addEventListener("click", function(){
+    console.log('olaa')
+}) */
+
