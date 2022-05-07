c4ac98443bb85920b2f9e9976e02119928fa000c
MoisesAzevedo <moisesdev2022@gmail.com>
2022-05-07 03:00:40 -0300

    add botão menu apps do google

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 
 
 
-function noticias(){
+function noticias(){            //remove o iframe
     if (document.querySelector('.janela-noticia') != null) {
         document.querySelector('.janela-noticia').remove();
     } 
 
-        /* document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> ` */
+        /* imprime o iframe */
         document.querySelector('body').innerHTML += ` 
             <div style="width: 100%" class="janela-noticia">
                 <button onclick="stopNews()"  class="botao2">Stop News</button> 
@@ -22,7 +22,6 @@ function noticias2(){
         document.querySelector('.janela-noticia').remove();
     }
 
-    /* document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> ` */
     document.querySelector('body').innerHTML += ` 
         <div style="width: 100%" class="janela-noticia">
             <button onclick="stopNews()" class="botao2">Stop News</button> 
@@ -35,7 +34,6 @@ function noticias3(){
         document.querySelector('.janela-noticia').remove();
     }
 
-    /* document.querySelector('head').innerHTML += ` <link rel="stylesheet" href="stopNews.css"> ` */
     document.querySelector('body').innerHTML += ` 
         <div style="width: 100%" class="janela-noticia">
             <button onclick="stopNews()"  class="botao2">Stop News</button> 
@@ -43,7 +41,28 @@ function noticias3(){
         </div> `
 }
 
-function stopNews(){
+function stopNews(){  //função do botão Stop News 
     document.querySelector('.janela-noticia').remove();
 }
 
+/* ======================================
+        Função botão google
+====================================== */
+
+function google(){
+    if (document.querySelector('.menu') != null ){
+        document.querySelector('.menu').remove();
+    } else {
+        document.querySelector('body').innerHTML += `
+        <div class="menu" >
+            <a href="https://docs.google.com/" target="_blank" class="icones">  
+                <div class="icones">
+                    <img src="https://www.gstatic.com/images/branding/product/1x/docs_2020q4_48dp.png">
+                    <p>Documentos</p>
+                </div>
+                <div class="icones-fundo">  </div>
+            </a> 
+        </div>
+        `
+    }
+}
