ace33f40d771255b1ffbeca86d6f7c4f64f54340
MoisesAzevedo <moisesdev2022@gmail.com>
2022-05-24 16:24:52 -0300

    corrigido problemas com menuConfig; Estilizado o menuConfig

--- js/config.js
+++ js/config.js
@@ -3,13 +3,18 @@ function config(){
         document.querySelector('.menuConfig').remove()
     } else {
         document.querySelector('body').innerHTML +=
-        `<div class="menuConfig">
+        `<div id="menu-edit-frase-fundo" onclick="menu_edit_off()" ></div> 
+         <div class="menuConfig">
             <div id="configLista">
-                <p onclick="planoFundo()">Plano de fundo</p>       
+                <p onclick="personalizar()">Personalização</p>       
+                <p onclick="siteFavorito()">Editar site favorito</p>       
             </div>
         
             <div id="configConteudo">
-                <h1 id="configConteudo-h1">Congurações</h1>
+                <div id="configInicio">
+                    <h1>Congurações</h1>
+                    <p > Selecione uma configuração no canto esquerdo da janela </p>
+                </div>
             </div>
         </div>`
     }
@@ -25,20 +30,29 @@ var monet = [
 
  console.log(monet)
 
-function planoFundo() {
-    if (document.getElementById('planoFundo') != null) {
+function personalizar() {
+    if (document.getElementById('personalizar') != null) {
         console.log("oi")
     } else {
-        document.getElementById('configConteudo-h1').remove();
+
+        if (document.getElementById('emBreve') != null) {
+            document.getElementById('emBreve').remove()
+        }
+        if (document.getElementById('configInicio') != null) {
+            document.getElementById('configInicio').remove()
+        }
+
         document.getElementById('configConteudo').innerHTML += 
         `
-        <div id="planoFundo">
+        <div id="personalizar">
             <h1>Alterar plano de fundo</h1>
 
             <h2>Monet:</h2>
-            <div class="imagens">
+            <div id="configScroll">
+                <div class="imagens">
 
-            </div>  
+                </div>  
+            </div>
         </div>`;
         
         var c =0
@@ -51,11 +65,26 @@ function planoFundo() {
             
             document.querySelector('.imagens').innerHTML += 
             `<div class="elemento" >
-            <img src="./img/Background/Monet/1500 x788/${c}.jpg" onclick="fundo${c}()">
+                 <img src="./img/Background/Monet/1500 x788/${c}.jpg" onclick="fundo${c}()">
             </div>`;
         }
     }
 
 }
 
-    
+function siteFavorito() {
+    if (document.getElementById('emBreve') != null) {
+        alert('Calma, já vai sair (kk)')
+    } else {
+
+        if (document.getElementById('personalizar') != null) {
+            document.getElementById('personalizar').remove()
+        }
+        if (document.getElementById('configInicio') != null) {
+            document.getElementById('configInicio').remove()
+        }
+
+        document.getElementById('configConteudo').innerHTML += 
+        `<p id="emBreve"> Disponível em breve </p>`
+    }
+}
--- js/frase.js
+++ js/frase.js
@@ -59,6 +59,8 @@ function menu_edit_off() {
         document.getElementById('menu-edit-frase').remove();
     } else if (document.getElementById('menu-edit-appGoogle') != null) {  //botão add apps do google
         userAppsGoogle_off() 
+    } else if (document.querySelector('.menuConfig') != null) {
+        document.querySelector('.menuConfig').remove()
     }
 }
 
