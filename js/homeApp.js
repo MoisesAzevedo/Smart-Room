d04715bb61e5e8a5367915e5b33c6833164aa12a
Moises Azevedo <moisesdev2022@gmail.com>
2022-07-25 10:20:54 -0300

    ADD: function editApp

--- js/homeApp.js
+++ js/homeApp.js
@@ -41,6 +41,7 @@ if (homeApp != null){
                 </div>
                 <div class="icones-fundo">  </div>
             </a> 
+            <p class='edit-app' onclick='editApp(${imprime})'>⁝</p>
         </div>`
     }
 } else {
@@ -108,6 +109,7 @@ function homeAdd_save(c) {
                 </div>
                 <div class="icones-fundo">  </div>
             </a> 
+            <p class='edit-app' onclick='editApp(${imprime})'>⁝</p>
         </div>`
     }
 
@@ -116,7 +118,85 @@ function homeAdd_save(c) {
 
 
 
-function homeApps_off() {
+function homeApps_off() {                               //frase.js
     document.querySelector('#menu-homeApp').remove()
 }   
 
+/* ========================================
+        Edit App
+======================================== */
+
+function editApp(imp) {
+    var menu_editApp =  `
+    "<div class="menu-editApp" onmousesout='off_editApp()'>
+        <button id='edit-editApp'>Editar</button>
+        <button id='exclui-editApp'>Excluir</button>
+    </div>,`
+ 
+
+    var homeApp = localStorage.getItem('homeApp')
+    if (homeApp != null){
+        var homeApp1 = JSON.parse(homeApp)
+        
+       /*  homeApp1.splice(imp, 0)
+
+        console.log(homeApp1)
+        localStorage.setItem('homeApp', JSON.stringify(homeApp1)) */
+    
+        for (remove in homeApp) {                           // Remove tudo
+            if (document.querySelector('.homeApp-user')){
+                document.querySelector('.homeApp-user').remove()            
+            }
+        }
+    
+        for (imprime in homeApp1) {                         //Cria tudo atualizado
+            
+            if (imprime == imp){            //tentando criar a condição para encontrar o array da questão.
+                document.querySelector('.homeApp-user-div').innerHTML += 
+                `<div class="homeApp-user">
+                    <a href="${homeApp1[imp].link}" target="_blank" class="icones">  
+                        <div class="icones-view">
+                            <img src="${homeApp1[imp].icone}">
+                            <p>${homeApp1[imp].nome}</p>
+                        </div>
+                        <div class="icones-fundo">  </div>
+                    </a> 
+                    <p class='edit-app' onclick='editApp(${imp})'>⁝</p>
+                    ${menu_editApp}
+                </div>
+               `
+            } else {
+                document.querySelector('.homeApp-user-div').innerHTML += 
+                `<div class="homeApp-user">
+                    <a href="${homeApp1[imprime].link}" target="_blank" class="icones">  
+                        <div class="icones-view">
+                            <img src="${homeApp1[imprime].icone}">
+                            <p>${homeApp1[imprime].nome}</p>
+                        </div>
+                        <div class="icones-fundo">  </div>
+                    </a> 
+                    <p class='edit-app' onclick='editApp(${imprime})'>⁝</p>
+                </div>`
+            }
+        }
+    } else {
+        var homeApp1 = []
+    }
+
+
+  /*   if (document.querySelector(".menu-editApp") != null){
+        document.querySelector('.menu-editApp').remove();
+    } */
+
+   /*  document.querySelector('.homeApp-user').innerHTML += `
+    <div class="menu-editApp" onmousesout='off_editApp()'>
+        <button id='edit-editApp'>Editar</button>
+        <button id='exclui-editApp'>Excluir</button>
+    </div>` */
+
+}
+
+function off_editApp(){
+    document.querySelector('.homeApp-user').remove()
+    console.log('op arapa')
+}
